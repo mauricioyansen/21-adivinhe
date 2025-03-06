be3f4b1ebe7f6cb8fbef9412d8f57ec085ac3a46
Mauricio <mauricio_cy@hotmail.com>
2025-03-06 00:27:50 -0300

    styles and initial structures

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import styles from "./app.module.css";
+import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import { LettersUsed } from "./components/LettersUsed";
+
+function App() {
+  function handleRestartGame() {
+    alert("Reiniciar o jogo");
+  }
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+
+        <Tip tip="Linguagem de programação dinâmica" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
+        </div>
+
+        <LettersUsed />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LettersUsed/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css";
+import { Letter } from "../Letter";
+
+export function LettersUsed() {
+  return (
+    <div className={styles.lettersUsed}>
+      <h5>Letras utilizadas</h5>
+
+      <div>
+        <Letter value="s" size="small" color="correct" />
+        <Letter value="x" size="small" color="wrong" />
+      </div>
+    </div>
+  );
+}
